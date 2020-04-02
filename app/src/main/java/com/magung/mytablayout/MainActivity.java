14570e72d09a5e48f6adb009e16acee705948adb
Agung <agung.maulana@fore.coffee>
2020-04-02 17:15:33 +0700

    fisrt commit

---
+++ app/src/main/java/com/magung/mytablayout/MainActivity.java
@@ -0,0 +1,59 @@
+package com.magung.mytablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private TabItem tab1, tab2, tab3, tab4;
+    private ViewPager viewPager;
+    private PageAdapter pageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        tab1 = (TabItem) findViewById(R.id.tab1);
+        tab2 = (TabItem) findViewById(R.id.tab2);
+        tab3 = (TabItem) findViewById(R.id.tab3);
+        tab4 = (TabItem) findViewById(R.id.tab4);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition() == 0) {
+                    pageAdapter.notifyDataSetChanged();
+                }else if (tab.getPosition() == 1) {
+                    pageAdapter.notifyDataSetChanged();
+                }else if (tab.getPosition() == 2) {
+                    pageAdapter.notifyDataSetChanged();
+                }else if (tab.getPosition() == 3) {
+                    pageAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/magung/mytablayout/PageAdapter.java
@@ -0,0 +1,43 @@
+package com.magung.mytablayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private int numoftabs;
+
+    public PageAdapter(@NonNull FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numoftabs = numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new tab1();
+            case 1:
+                return new tab2();
+            case 2:
+                return new tab3();
+            case 3:
+                return new tab4();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numoftabs;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
+}
